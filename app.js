4df13bdb05fb9dfed3ef85fb6978a56a5b703980
Jesus Duarte <jduartedsp@gmail.com>
2023-06-28 07:01:44 -0400

    test

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const User = require('./models/user');
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.e7urz.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
 const app = express();
 const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions',
+    uri: MONGODB_URI,
+    collection: 'sessions',
 });
 const csrfProtection = csrf();
 
@@ -26,24 +26,24 @@ const csrfProtection = csrf();
 // const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  },
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 app.set('view engine', 'ejs');
@@ -53,51 +53,51 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, 'access.log'),
-  { flags: 'a' },
+    path.join(__dirname, 'access.log'),
+    { flags: 'a' },
 );
 app.use(helmet());
 app.use(compression());
 app.use(morgan('short'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'),
 );
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  }),
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    }),
 );
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use((req, res, next) => {
-  // throw new Error('Sync Dummy');
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      if (!user) {
+    // throw new Error('Sync Dummy');
+    if (!req.session.user) {
         return next();
-      }
-      req.user = user;
-      next();
-    })
-    .catch(err => {
-      next(new Error(err));
-    });
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            next(new Error(err));
+        });
 });
 
 app.use('/admin', adminRoutes);
@@ -109,22 +109,21 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  // res.status(error.httpStatusCode).render(...);
-  // res.redirect('/500');
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session.isLoggedIn,
-  });
+    // res.redirect('/500');
+    res.status(500).render('500', {
+        pageTitle: 'Error!',
+        path: '/500',
+        isAuthenticated: req.session.isLoggedIn,
+    });
 });
 
 mongoose
-  .connect(MONGODB_URI)
-  .then(result => {
-    // https
-    //   .createServer({ key: privateKey, cert: certificate }, app)
-    app.listen(process.env.PORT || 3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .connect(MONGODB_URI)
+    .then(result => {
+        // https
+        //   .createServer({ key: privateKey, cert: certificate }, app)
+        app.listen(process.env.PORT || 3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
