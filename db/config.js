cad34bf411cb55941968ff08dd6df2eea7a85222
Jesus Duarte <jduartedsp@gmail.com>
2023-08-02 12:52:39 -0400

    migration working back again

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
-const config = require('../config/config');
+import config from '../config/config.js'
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-module.exports = {
+export default {
   development: {
     url: URI,
     dialect: 'postgres',
