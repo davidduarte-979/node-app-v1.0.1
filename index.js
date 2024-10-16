a24ef72a6b3101bc2640cf0e141d506226c89055
Jesus Duarte <jduartedsp@gmail.com>
2024-10-16 17:16:52 -0400

    added also local to received incoming request

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.use(cors(corsOptionsDelegate));
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'https://www.davidduarte.dev');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
 
   // Request methods you wish to allow
   res.setHeader(
