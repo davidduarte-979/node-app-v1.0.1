02bdc4f2b30fc000ee619175296620bd675b10f0
Jesus Duarte <jduartedsp@gmail.com>
2024-10-16 18:11:43 -0400

    why you are not working

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const corsOptionsDelegate = function (req, callback) {
   console.log(req.header('Origin'));
 
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: false }; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: true }; // disable CORS for this request
+    corsOptions = { origin: false }; // disable CORS for this request
   }
   callback(null, corsOptions); // callback expects two parameters: error and options
 };
