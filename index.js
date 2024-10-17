7b2f1324e811e2198395d3d22162a8d14d562e48
Jesus Duarte <jduartedsp@gmail.com>
2024-10-17 11:47:27 -0400

    again the cors

--- index.js
+++ index.js
@@ -16,12 +16,32 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://www.davidduarte.dev',
+    origin: '*',
     headers: ['Content-Type'],
-    credentials: false,
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT'],
+    credentials: true,
   })
 );
 app.options('*', cors());
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    'http://localhost:4200',
+    'https://www.davidduarte.dev',
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
+  next();
+});
 
 // auth and session middlerwares
 import './util/auth/index.js';
