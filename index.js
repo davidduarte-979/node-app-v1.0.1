7a1860074b18ebe866aac589b065e4713d7d6b28
Jesus Duarte <jduartedsp@gmail.com>
2025-05-24 08:50:20 -0400

    remove wildcard origin from CORS configuration

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
   const allowedOrigins = [
     'http://localhost:4200',
     'https://www.davidduarte.dev',
